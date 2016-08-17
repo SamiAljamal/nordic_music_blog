b2ed46ddfa98b9518aa1b41d787ec5a4875f5c2a
Maarja Laineste and Sami Al-Jamal <maarja+sami@>
2016-08-17 16:03:47 -0700

    able to update blogpost

---
+++ app/components/update-blogpost.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateBlogpostForm: false,
+  actions: {
+    updateBlogpostForm() {
+      this.set('updateBlogpostForm', true);
+    },
+    update(article) {
+      var params = {
+        title: this.get('title'),
+        date: this.get('date'),
+        image: this.get('image'),
+        body: this.get('body'),
+        introduction: this.get('introduction'),
+      };
+      this.set('updateBlogpostForm', false);
+      this.sendAction('update', blogpost, params);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('blogpost');
+  },
+  actions: {
+    save(params){
+      var newBlogPost = this.store.createRecord('blogpost', params);
+      newBlogPost.save();
+      this.transitionTo('index');
+    }
   }
 });
